ad53f314eb871420907a3eba7a9ff3272ad4ae95
unmask26 <unmasksupport@gmail.com>
2026-08-04 09:48:53 +0300

    chore(release): bump version to 2.4.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "unmask.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 245
-        versionName = "2.4.5"
+        versionCode = 246
+        versionName = "2.4.6"
     }
 
     signingConfigs {
